763376b0414f23613cb83ba97ec210c766e7d062
Micha Hanselmann <micha.hanselmann@gmail.com>
2022-08-29 20:13:17 +0200

    test: midi message

--- src/base/midi_message.rs
+++ src/base/midi_message.rs
@@ -1,5 +1,5 @@
 /// MIDI message type.
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum MidiMessage {
     /// Control change: channel, control number, value.
     ControlChange(u8, u8, u8),
@@ -23,5 +23,32 @@ impl MidiMessage {
         } else {
             None
         }
+    }
+}
+
+/// Unit tests.
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn from_bytes() {
+        let note_off = MidiMessage::from_bytes(&[0x81, 0x48, 0x12]);
+        assert_eq!(note_off, Some(MidiMessage::NoteOff(0x01, 0x48, 0x12)));
+
+        let note_on = MidiMessage::from_bytes(&[0x9A, 0x52, 0x24]);
+        assert_eq!(note_on, Some(MidiMessage::NoteOn(0x0A, 0x52, 0x24)));
+
+        let control_change = MidiMessage::from_bytes(&[0xB3, 0x12, 0x36]);
+        assert_eq!(control_change, Some(MidiMessage::ControlChange(0x03, 0x12, 0x36)));
+
+        let invalid = MidiMessage::from_bytes(&[0x01, 0x12, 0x36]);
+        assert_eq!(invalid, None);
+
+        let invalid = MidiMessage::from_bytes(&[0x81, 0x48, 0x12, 0x01]);
+        assert_eq!(invalid, None);
+
+        let invalid = MidiMessage::from_bytes(&[0x81]);
+        assert_eq!(invalid, None);
     }
 }
